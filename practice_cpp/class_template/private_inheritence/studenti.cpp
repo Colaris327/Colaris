b2e94bd68e42d00c7fbe78c1ec1c713d46c00076
Colaris327 <92642967+Colaris327@users.noreply.github.com>
2021-12-15 22:35:15 +0800

    Add files via upload

---
+++ practice_cpp/class_template/private_inheritence/studenti.cpp
@@ -0,0 +1,70 @@
+#include "studdenti.h"
+using std::ostream;
+using std::istream;
+using std::endl;
+using std::string;
+
+double Student::Average() const
+{
+    if (ArrayDb::size() > 0)
+        return ArrayDb::sum() / ArrayDb::size();
+    else
+        return 0;
+}
+
+const string & Student::Name() const
+{
+    return (const string &) *this;
+}
+
+double & Student::operator[](int i)
+{
+    return ArrayDb::operator[](i); // use valarray<double>::operator[]()
+}
+
+double Student::operator[](int i) const
+{
+    return ArrayDb::operator[](i);
+}
+
+ostream & Student::arr_out(ostream & os) const
+{
+    int i;
+    int lim = ArrayDb::size();
+    if (lim > 0)
+    {
+        for (i = 0; i < lim; i++)
+        {
+            os << ArrayDb::operator[](i) << " ";
+            if (i % 5 == 4)
+                os << endl;
+        }
+        if (i % 5 != 0)
+            os << endl;
+    }
+    else
+        os << "empty array ";
+    return os;
+}
+
+// friend
+// use string version of operator>>()
+istream & operator>>(istream & is, Student & stu)
+{
+    is >> (string &)stu;
+    return is;
+}
+
+// use string friend getline(ostream &, const string &)
+istream & getline(istream & is, Student & stu)
+{
+    getline(is, (string &)stu);
+    return is;
+}
+
+ostream & operator<<(ostream & os, const Student & stu)
+{
+    os << "Scores for " << (const string &)stu << ": \n";
+    stu.arr_out(os); // use private method to simplify
+    return os;
+}
---
+++ practice_cpp/class_template/private_inheritence/use_stui.cpp
@@ -0,0 +1,44 @@
+// page 489
+// compile with student.cpp
+#include "studdenti.h"
+#include <iostream>
+using std::cin;
+using std::cout;
+using std::endl;
+
+void set(Student & sa, int n);
+
+const int pupils = 3;
+const int quizzes = 5;
+
+int main()
+{
+    Student ada[pupils] = {Student(quizzes), Student(quizzes), Student(quizzes)};
+    int i;
+    for (i = 0; i < pupils; i++)
+        set(ada[i], quizzes);
+    cout << "\nStudent List:\n";
+    for (i = 0; i < pupils; i++)
+        cout << ada[i].Name() << endl;
+    cout << "\nResults:";
+    for (i = 0; i < pupils; i++)
+    {
+        cout << endl << ada[i];
+        cout << "average: " << ada[i].Average() << endl;
+    }
+    cout << "Done\n";
+    return 0;
+}
+
+void set(Student & sa, int n)
+{
+    cout << "Enter the student's name: ";
+    getline(cin, sa);
+    cout << "Enter " << n << " quiz scores: ";
+    for (int i = 0; i < n; i++)
+    {
+        cin >> sa[i];
+    }
+    while (cin.get() != '\n')
+        continue;
+}
